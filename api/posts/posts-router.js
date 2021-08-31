8d8e129677af797c84ec8a11ef506864855bf264
Joey_M_Brown <joey.ma.brown@gmail.com>
2021-08-31 16:42:42 -0400

    Finished scaffolding routes in router

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,32 @@
 // implement your posts router here
+const express = require('express');
+const router = express.Router();
+const Posts = require('./posts-model');
+
+router.get('/', (req, res) => {
+    res.status(200).json({ message: 'Returns an array of all post objects'})
+})
+
+router.get('/:id', (req, res) => {
+    res.status(200).json({ message: 'returns the post object with the specified id'})
+})
+
+router.get('/:id/comments', (req, res) => {
+    res.status(200).json({ message: 'returns array of all comments with post id'})
+})
+
+
+router.post('/', (req, res) => {
+    res.status(200).json({ message: 'Creates a post, req.body returns newly created post'})
+})
+
+
+router.put('/:id', (req, res) => {
+    res.status(200).json({ message: 'Updates post using id, and data from req.body, returns modified post.'})
+})
+
+
+router.delete('/:id', (req, res) => {
+    res.status(200).json({ message: 'Removes post at specified id, returns deleted post'})
+})
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,2 +1,19 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express');
+const postsRouter = require('./posts/posts-router');
+
+const server = express();
+server.use(express.json());
+
+server.use('/api/posts', postsRouter);
+
+server.use('*', (req, res) => {
+    res.status(404).json({ message: 'sorry, no route found!'}) //Order matters!
+})
+
+// server.get('/', (req, res) => {
+//     res.status(200).json({message: 'server working homie'})
+// })
+
+module.exports = server;
